90e145b11e70da26dc8e3a73f5366495d89f95c4
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-23 23:36:50 +0530

    Added : Retrive where coause Between,CAST and Now()

--- payroll_service.sql
+++ payroll_service.sql
@@ -14,9 +14,13 @@ primary key(id)
 );
 /* Insert into employee_payroll */
 insert into employee_payroll (name,salary,start_date) values("Nagraj",25000,'2018-01-01');
+insert into employee_payroll (name,salary,start_date) values("Manoj",28000,'2019-01-01'),("Prem",38000,'2020-08-23');
 
 /* Show table */
 Select * from employee_payroll;
+
+Select * from employee_payroll where name = "Nagraj";
+Select * from employee_payroll where start_date BETWEEN CAST('2018-01-01'AS DATE) AND DATE(NOW());
 
 
 
