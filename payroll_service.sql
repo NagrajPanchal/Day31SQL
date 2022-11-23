d40acbbae9fabe9743fd7a360ec284f81057298e
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-23 23:09:03 +0530

    Added : Craete table employee payroll

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,3 +1,14 @@
+/* Create database payroll_service */
 create database payroll_service;
+/* show data base */
 show databases;
+/* use data base */
 use payroll_service;
+/* create Table Employee_Payroll */
+create table employee_payroll (
+id int auto_increment, 
+name varchar(20),
+salary int,
+start_data date,
+primary key(id)
+);
